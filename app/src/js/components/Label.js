acaea73429ba773f1fa2b5717f65aa036cf591bf
andrewdamelio <dev@andrewdamel.io>
2015-02-06 20:04:58 -0500

    moar greensocks

--- app/src/js/components/Label.js
+++ app/src/js/components/Label.js
@@ -1,15 +1,27 @@
 'use strict';
 
 var React = require('react');
+var TweenMax = require('../core/TweenMax');
 
 var Label = React.createClass({
   handleImageChange: function() {
     this.props.changeImage(this.props.image);
   },
+  componentDidUpdate: function(nextProps, nextState) {
+    var element = this.refs.image.getDOMNode();
+    var t = new TimelineMax();
+
+    t.to(element, 0.3, {rotationY: 180});
+    t.to(element, 0.3, {rotationY: -180});
+  },
   render: function() {
     return (
       <div className="ui image label">
-        <img src={this.props.image} onClick={this.handleImageChange}/>
+        <img ref="image"
+             src={this.props.image}
+             onClick={this.handleImageChange}
+             onMouseEnter={this.zoomIn}
+             onMouseLeave={this.zoomOut} />
         <a href={this.props.link}>{this.props.user}</a>
       </div>
     );
--- app/src/js/components/Logo.js
+++ app/src/js/components/Logo.js
@@ -1,17 +1,35 @@
 'use strict';
 
-var React = require('react');
+var React = require('react/addons');
+var PureRenderMixin = require('react').addons.PureRenderMixin;
 
 var Logo = React.createClass({
+  mixins: [PureRenderMixin],
   getDefaultProps: function(){
     return {
       logo: 'dist/images/reactjs.png'
     };
   },
+  zoomIn: function() {
+    var element = this.refs.image.getDOMNode();
+    var t = new TimelineMax();
+    t.to(element, 0.1, {scaleX: 1.1, scaleY: 1.1, ease: Circ.easeOut});
+    t.to(element, 0.5, {rotation: 1080, repeat: -1});
+  },
+  zoomOut: function() {
+    var element = this.refs.image.getDOMNode();
+    var t = new TimelineMax();
+    t.to(element, 0.0, {rotation: -1080});
+    t.to(element, 0.0, {scaleX: 1.0, scaleY: 1.0});
+  },
   render: function() {
     return (
       <div className="logo">
-        <img src={this.props.logo} className="ui small bordered circular image"/>
+        <img ref="image"
+             src={this.props.logo}
+             className="ui small bordered circular image"
+             onMouseEnter={this.zoomIn}
+             onMouseLeave={this.zoomOut} />
       </div>
     );
   }
